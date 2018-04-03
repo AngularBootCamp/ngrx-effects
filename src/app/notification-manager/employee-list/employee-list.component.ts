a1bf2208bdae8d8d202a02e9ba4f38b615ef07f2
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-04-03 11:12:14 -0500

    curriculum update

--- src/app/notification-manager/employee-list/employee-list.component.ts
+++ src/app/notification-manager/employee-list/employee-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 import { AppState } from '../../state';
 import { AckEmployeeAction } from './../../employees.state';
